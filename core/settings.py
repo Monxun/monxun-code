17703014f7ef0c67118e4a89cc438131d9809b3a
Monxun <87790334+Monxun@users.noreply.github.com>
2021-12-08 14:04:33 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'widget_tweaks',
 
     # AllAuth
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
+#     'allauth',
+#     'allauth.account',
+#     'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -81,14 +81,14 @@ TEMPLATES = [
 ]
 
 # ALL AUTH CONFIG
-AUTHENTICATION_BACKENDS = [
+# AUTHENTICATION_BACKENDS = [
 
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
+#     # Needed to login by username in Django admin, regardless of `allauth`
+#     'django.contrib.auth.backends.ModelBackend',
 
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
+#     # `allauth` specific authentication methods, such as login by e-mail
+#     'allauth.account.auth_backends.AuthenticationBackend',
+# ]
 
 
 SITE_ID = 1
