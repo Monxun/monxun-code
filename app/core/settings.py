05c120618f159844bb160c1ab101884b377bdf87
Monxun <michaelgibney1@gmail.com>
2022-01-22 00:07:52 -0700

    updated docker

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,6 +1,5 @@
 
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,7 +16,7 @@ SECRET_KEY = 'django-insecure-lr&ku5&pew+5w)6=eri*@j5(sg1&v&$_=x=o6f$l4_9jt$5!9(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.monxuncode.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django.monxuncode.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -107,17 +106,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'monxun',
-        'USER': 'postgres',
-        'PASSWORD': 'Tesla5439',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 
 
 # Password validation
@@ -188,57 +180,57 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ####################################################################
 # HEROKU
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
 
 
-####################################################################
-# MEMECACHIER CONFIG(Heroku)
+# ####################################################################
+# # MEMECACHIER CONFIG(Heroku)
 
-def get_cache():
-  import os
-  try:
-    servers = os.environ['MEMCACHIER_SERVERS']
-    username = os.environ['MEMCACHIER_USERNAME']
-    password = os.environ['MEMCACHIER_PASSWORD']
-    return {
-      'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-        # TIMEOUT is not the connection timeout! It's the default expiration
-        # timeout that should be applied to keys! Setting it to `None`
-        # disables expiration.
-        'TIMEOUT': None,
-        'LOCATION': servers,
-        'OPTIONS': {
-          'binary': True,
-          'username': username,
-          'password': password,
-          'behaviors': {
-            # Enable faster IO
-            'no_block': True,
-            'tcp_nodelay': True,
-            # Keep connection alive
-            'tcp_keepalive': True,
-            # Timeout settings
-            'connect_timeout': 2000, # ms
-            'send_timeout': 750 * 1000, # us
-            'receive_timeout': 750 * 1000, # us
-            '_poll_timeout': 2000, # ms
-            # Better failover
-            'ketama': True,
-            'remove_failed': 1,
-            'retry_timeout': 2,
-            'dead_timeout': 30,
-          }
-        }
-      }
-    }
-  except:
-    return {
-      'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-      }
-    }
+# def get_cache():
+#   import os
+#   try:
+#     servers = os.environ['MEMCACHIER_SERVERS']
+#     username = os.environ['MEMCACHIER_USERNAME']
+#     password = os.environ['MEMCACHIER_PASSWORD']
+#     return {
+#       'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+#         # TIMEOUT is not the connection timeout! It's the default expiration
+#         # timeout that should be applied to keys! Setting it to `None`
+#         # disables expiration.
+#         'TIMEOUT': None,
+#         'LOCATION': servers,
+#         'OPTIONS': {
+#           'binary': True,
+#           'username': username,
+#           'password': password,
+#           'behaviors': {
+#             # Enable faster IO
+#             'no_block': True,
+#             'tcp_nodelay': True,
+#             # Keep connection alive
+#             'tcp_keepalive': True,
+#             # Timeout settings
+#             'connect_timeout': 2000, # ms
+#             'send_timeout': 750 * 1000, # us
+#             'receive_timeout': 750 * 1000, # us
+#             '_poll_timeout': 2000, # ms
+#             # Better failover
+#             'ketama': True,
+#             'remove_failed': 1,
+#             'retry_timeout': 2,
+#             'dead_timeout': 30,
+#           }
+#         }
+#       }
+#     }
+#   except:
+#     return {
+#       'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+#       }
+#     }
 
-CACHES = get_cache()
+# CACHES = get_cache()
